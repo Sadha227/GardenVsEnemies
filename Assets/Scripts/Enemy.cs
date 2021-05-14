3ac2f8945bab6e63c47f47a370ae23f3f970cdb6
pp <varvarchik3@gmail.com>
2021-05-14 17:46:45 +0300

    Add enemy spawner script and object, add enemy script.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] Animator animator;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+    public void SetMoveSpeed(float speed) { moveSpeed = speed; }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnTime = 1f;
+    [SerializeField] float maxSpawnTime = 5f;
+    [SerializeField] Enemy[] enemyPrefabArray;
+    bool spawn = true;
+
+
+    IEnumerator Start() 
+    {
+        while(spawn) { yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime)); SpawnAttackers(); }
+    }
+
+    private void SpawnAttackers()
+    {
+        int enemuIndex = Random.Range(0, enemyPrefabArray.Length);
+        Spawn(enemyPrefabArray[enemuIndex]);
+    }
+
+    private void Spawn(Enemy enemy) { Enemy newEnemy = Instantiate(enemy, transform.position, Quaternion.identity); }
+    public void StopSpawn() { spawn = false; }
+}
