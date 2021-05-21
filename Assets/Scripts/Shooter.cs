6a7e950ed8a132aaf7374380b8decaeb05bfe882
pp <varvarchik3@gmail.com>
2021-05-21 18:29:37 +0300

    😎Implement abiliti to deal damage to the enemies by add Health, add Shooter.cs😎

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectilePrefabs;
+    [SerializeField] GameObject gun;
+    [SerializeField] Animator animator;
+  //  [SerializeField] int damage;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       // Fire();
+    }
+    public void Fire() { var projectile = Instantiate(projectilePrefabs, gun.transform.position, Quaternion.identity); }
+}
